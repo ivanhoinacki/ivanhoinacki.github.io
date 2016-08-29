87ac35a4a78dfd52836eb020ddb735857d023ca5
Ivan Augusto Hoinacki <ivanhweb@gmail.com>
2016-08-29 11:57:26 -0300

    update

--- app/components/resume/resume.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-    selector: 'resume',
-    templateUrl: 'app/components/resume/resume.html',
-    styleUrls: ['app/components/resume/resume.css']
-})
-
-export class ResumeCmp {}
