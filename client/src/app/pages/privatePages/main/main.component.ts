5ddd50cd38a2018ddedf1a2c6b49b29507791fb6
Ivan A. Hoinacki <ivanhweb@gmail.com>
2020-03-26 13:58:21 -0300

    Update resume

--- client/src/app/pages/privatePages/main/main.component.ts
+++ client/src/app/pages/privatePages/main/main.component.ts
@@ -1,21 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-declare var $ : any;
+import { Component, OnInit } from "@angular/core";
+declare var $: any;
 
 @Component({
-    selector: 'main',
-    templateUrl: './main.component.html',
-    styleUrls: ['./main.component.scss']
+    selector: "main",
+    templateUrl: "./main.component.html",
+    styleUrls: ["./main.component.scss"]
 })
-
 export class MainComponent implements OnInit {
-
     constructor() {}
 
     ngOnInit() {
-        $('.description').typed({
-            strings: [
-                'web deev..', 'Noow Team Leader ;)'
-            ],
+        $(".description").typed({
+            strings: ["web deev..", "Full Stack developer ;)"],
             backDelay: 300,
             typeSpeed: 30
         });
